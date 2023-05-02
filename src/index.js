9abe8bf1297e91d23fa64a97b655b6d291ea9a74
Nasser Sheikh <nassershikh0@gmail.com>
2023-05-02 14:14:03 +0300

    edit index.js file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Html from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascrpt";
+
+//routing
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: (
+      <h2 style={{ color: "black", textAlign: "center", marginTop: "100px" }}>
+        Sorry...................
+      </h2>
+    ),
+  },
+  {
+    path: "/html",
+    element: <Html />,
+  },
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
